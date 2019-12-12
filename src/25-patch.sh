6a6640739c3393e107bfb3fc89a59bd7cb16b0b1
buraksarac <burak@linux.com>
2019-12-12 02:38:26 +0100

    scripts until chroot

---
+++ src/25-patch.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+
+source $(pwd)/commons.sh
+
+echo "checking user"
+
+checkme "$INSTALL_USER" || { echo "Invalid user expected $INSTALL_USER or INSTALL_USER not set" ; exit 1; }
+checkVar $MAKEFLAGS ||  { echo "Please set MAKEFLAGS before using this script" ; exit 1; }
+checkVar $INSTALL_LOC ||  { echo "Please set INSTALL_LOC before using this script" ; exit 1; }
+checkVar $INSTALL_TGT ||  { echo "Please set INSTALL_TGT before using this script" ; exit 1; }
+
+export GNULIB_SRCDIR=$(pwd)/gnulib
+
+echo "cloning repo"
+git clone https://github.com/qunixorg/patch.git || { echo "can not fetch repository, please check logs installation aborted" ; exit 1; }
+
+pushd patch
+
+checkUpstream
+
+./bootstrap || { echo "can not run bootstrap, please check logs installation aborted" ; exit 1; }
+
+./configure --prefix=/tools || { echo "can not configure project, please check logs installation aborted" ; exit 1; }
+
+time make -s > make.log || { echo "can not make project, please check logs installation aborted" ; exit 1; }
+time make -s install > install.log || { echo "can not install, please check logs installation aborted" ; exit 1; }
+
+popd
---
+++ src/27-python.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+
+source $(pwd)/commons.sh
+
+echo "checking user"
+
+checkme "$INSTALL_USER" || { echo "Invalid user expected $INSTALL_USER or INSTALL_USER not set" ; exit 1; }
+checkVar $MAKEFLAGS ||  { echo "Please set MAKEFLAGS before using this script" ; exit 1; }
+checkVar $INSTALL_LOC ||  { echo "Please set INSTALL_LOC before using this script" ; exit 1; }
+checkVar $INSTALL_TGT ||  { echo "Please set INSTALL_TGT before using this script" ; exit 1; }
+
+export GNULIB_SRCDIR=$(pwd)/gnulib
+
+echo "cloning repo"
+git clone https://github.com/qunixorg/cpython.git || { echo "can not fetch repository, please check logs installation aborted" ; exit 1; }
+
+pushd cpython
+
+checkUpstream
+
+sed -i '/def add_multiarch_paths/a \ \ \ \ \ \ \ \ return' setup.py
+
+./configure --prefix=/tools --without-ensurepip
+
+time make -s > make.log || { echo "can not make project, please check logs installation aborted" ; exit 1; }
+
+time make -s install > install.log || { echo "can not install, please check logs installation aborted" ; exit 1; }
+
+popd
