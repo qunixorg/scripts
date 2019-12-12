6a86ae15209528438cea5559a14c284aad99546c
buraksarac <burak@linux.com>
2019-12-12 19:28:41 +0100

    run script

--- 01-src/RUNALL.sh
+++ 01-src/RUNALL.sh
@@ -2,5 +2,5 @@
 
 for prefix in {1..34}
 do
-	ls -la "$prefix"-*sh || { echo "can not run script $prefix, please check logs installation aborted, after you fix run later scripts manually" ; exit 1; }
+	/bin/sh "$prefix"-*sh || { echo "can not run script $prefix, please check logs installation aborted, after you fix run later scripts manually" ; exit 1; }
 done	
